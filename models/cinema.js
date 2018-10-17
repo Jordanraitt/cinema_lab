fcb7dafcf7cff1986f33a586dba71e3be7082ca3
jordanraitt <jordanraitt@hotmail.com>
2018-10-17 14:53:16 +0100

    NOW done part 4

--- models/cinema.js
+++ models/cinema.js
@@ -14,4 +14,8 @@ Cinema.prototype.filterByGenre = function(genre){
   return this.films.filter(film => film.genre === genre);
 }
 
+Cinema.prototype.checkByYear = function(year){
+  return this.films.some(film => film.year === year)
+}
+
 module.exports = Cinema;
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -47,7 +47,13 @@ describe('Cinema', function () {
   });
 
 
-  it('should be able to check whether there are some films from a particular year');
+  it('should be able to check whether there are some films from a particular year', function() {
+    const actual = cinema.checkByYear(2016);
+    const expected = true;
+    assert.equal(actual, expected);
+  });
+
+
   it('should be able to check whether there are no films from a particular year');
   it('should be able to check whether all films are over a particular length');
   it('should be able to calculate total running time of all films');
