749f3a1d6df9e77aef85cf64dff547ba437ed2f0
jordanraitt <jordanraitt@hotmail.com>
2018-10-17 15:52:12 +0100

    extension completed

--- models/cinema.js
+++ models/cinema.js
@@ -8,32 +8,28 @@ Cinema.prototype.listTitles = function(){
 
 Cinema.prototype.findByTitle = function(film){
   return this.films.find(function(n) {return n === film} );
-}
+};
 
 Cinema.prototype.checkByYear = function(year){
   return this.films.some(film => film.year === year)
-}
+};
 
 Cinema.prototype.allCheckLength = function(length){
   return this.films.every(film => film.length > length);
-}
+};
 
 Cinema.prototype.timeOfAllFilms = function(){
   const reducer = (a, film) => a + film.length
   return this.films.reduce(reducer, 0)
-}
-
-
-
-
+};
 
 Cinema.prototype.filmsByProperty = function(name, value){
-if (name === year){
 
-  return this.films.filter(film => film.year === value);
-  }
-  // else if (name === year)
+  return this.films.filter((film) => {
+    return film[name] === value;
+  });
 }
+  // else if (name === year)
 
 
 
